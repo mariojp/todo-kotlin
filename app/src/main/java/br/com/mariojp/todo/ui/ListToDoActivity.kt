8ce0157194eef91e41ceb51448b5cbdf5eae202c
Mario Pereira <554178+mariojp@users.noreply.github.com>
2022-11-23 19:16:26 -0300

    Parte 3 - Biblioteca Data Biding

--- app/src/main/java/br/com/mariojp/todo/ui/ListToDoActivity.kt
+++ app/src/main/java/br/com/mariojp/todo/ui/ListToDoActivity.kt
@@ -1,20 +1,23 @@
 package br.com.mariojp.todo.ui
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import br.com.mariojp.todo.R
+import br.com.mariojp.todo.databinding.ActivityListTodoBinding
 
 class ListToDoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_todo)
-        val id = findViewById<TextView>(R.id.activity_list_todo_id)
-        id.setText("1")
-        val title = findViewById<TextView>(R.id.activity_list_todo_title)
-        title.setText("Estudar kotlin")
-        val description = findViewById<TextView>(R.id.activity_list_todo_description)
-        description.setText("EStudar kotlin para Mobile")
+        val binding = ActivityListTodoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.activityListTodoId.setText("1")
+
+        binding.activityListTodoTitle.setText("Estudar kotlin")
+
+        binding.activityListTodoDescription.setText("Estudar kotlin para Mobile")
 
     }
 }
